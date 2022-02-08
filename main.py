93d3247e2706c16ba8c2730120ba6e835e584a52
Лена <starostina.lena05@gmail.com>
2022-02-08 21:55:50 +0400

    Первая дополнительная задача

--- main.py
+++ main.py
@@ -272,5 +272,69 @@ def results(nickname, level, rating):
                 </html>'''
 
 
+@app.route('/load_photo', methods=['POST', 'GET'])
+def load_photo():
+    if request.method == 'GET':
+        return f'''<!doctype html>
+                        <html lang="ru">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet"
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                            crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Отбор астронавтов</title>
+                          </head>
+                          <body>
+                            <h2>Загрузка фотографии</h2>
+                            <h3>для участия в миссии</h3>
+                            <div>
+                                <form class="load_photo_form" method="post" enctype="multipart/form-data">
+                                    <div class="form-group">
+                                        <label for="photo">Приложите фотографию</label>
+                                        <input type="file" class="form-control-file" id="photo" name="file">
+                                    </div>
+                                    <br>
+                                    <button type="submit" class="btn btn-info">Отправить</button>
+                                </form>
+                            </div>
+                          </body>
+                        </html>'''
+    elif request.method == 'POST':
+        request.files['file'].save('static/img/user_photo.jpg')
+        return f'''<!doctype html>
+                                <html lang="ru">
+                                  <head>
+                                    <meta charset="utf-8">
+                                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                                    <link rel="stylesheet"
+                                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                                    crossorigin="anonymous">
+                                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                                    <title>Отбор астронавтов</title>
+                                  </head>
+                                  <body>
+                                    <h2>Загрузка фотографии</h2>
+                                    <h3>для участия в миссии</h3>
+                                    <div>
+                                        <form class="load_photo_form" method="post" enctype="multipart/form-data">
+                                            <div class="form-group">
+                                                <label for="photo">Приложите фотографию</label>
+                                                <input type="file" class="form-control-file" id="photo" name="file">
+                                            </div>
+                                            <br>
+                                            <img src="{url_for('static', filename='img/user_photo.jpg')}" 
+                                            alt="Похоже, при загрузке картинки что-то пошло не так" width=200 height=200>
+                                            <br>
+                                            <button type="submit" class="btn btn-info">Отправить</button>
+                                        </form>
+                                    </div>
+                                  </body>
+                                </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
