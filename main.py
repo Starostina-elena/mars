e67df178e776d95627b01effd2ce99f541526eb2
Лена <starostina.lena05@gmail.com>
2022-02-08 22:27:49 +0400

    2 дополнительная задача

--- main.py
+++ main.py
@@ -336,5 +336,61 @@ def load_photo():
                                 </html>'''
 
 
+@app.route('/carousel')
+def carousel():
+    return f'''<!doctype html>
+                <html lang="ru">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet"
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                            crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <script src="https://getbootstrap.com/docs/5.1/dist/js/bootstrap.bundle.min.js"
+                    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+                    crossorigin="anonymous"></script>
+                    <script src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"></script>
+                    <script src="https://getbootstrap.com/docs/5.1/assets/js/docs.min.js"></script>
+                    <title>Пейзажи Марса</title>
+                  </head>
+                  <body>
+                    <h1 class=formatting_v_2>Пейзажи Марса</h1>
+                    <br>
+                    <div class="wrapper-carousel">
+                        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-indicators">
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                            </div>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="{url_for('static', filename='img/mars_carousel_1.jpg')}" width=320 height=240
+                                    class="d-block w-100" alt="Здесь должна была быть картинка, но не нашлась">
+                                </div>
+                                <div class="carousel-item">
+                                  <img src="{url_for('static', filename='img/mars_carousel_2.jpg')}" width=360 height=225
+                                  class="d-block w-100" alt="Здесь должна была быть картинка, но не нашлась">
+                                </div>
+                                <div class="carousel-item">
+                                  <img src="{url_for('static', filename='img/mars_carousel_3.jpg')}" width=341 height=256
+                                  class="d-block w-100" alt="Здесь должна была быть картинка, но не нашлась">
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                    </div>
+                  </body>
+                </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
