e43e3413c788e91995f6e5ad49d49747ac184cf6
Лена <starostina.lena05@gmail.com>
2022-02-14 17:57:25 +0400

    flask шаблоны 4 задача из классной

--- prepare_for_mission.py
+++ prepare_for_mission.py
@@ -23,5 +23,21 @@ def list_prof(type_list):
         return 'Неверный параметр'
 
 
+@app.route('/answer')
+@app.route('/auto_answer')
+def auto_answer():
+    params = {
+        'title': 'Анкета',
+        'surname': 'Watny',
+        'name': 'Mark',
+        'education': 'Выше среднего',
+        'profession': 'штурман марсохода',
+        'sex': 'male',
+        'motivation': 'Всегда мечтал застрять на Марсе!',
+        'ready': True
+    }
+    return render_template('auto_answer.html', **params)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
