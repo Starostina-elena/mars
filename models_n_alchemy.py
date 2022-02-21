86d8dad97cec81e0f47f2aec6edbf19312f4de49
Лена <starostina.lena05@gmail.com>
2022-02-21 18:09:24 +0400

    алхимия 1 и 2 задача из классной

---
+++ models_n_alchemy.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, redirect
+from data import db_session
+from data.users import User
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    db_session.global_init("db/users_and_jobs.db")
+    session = db_session.create_session()
+    user = User()
+    user.surname = "Scott"
+    user.name = "Ridley"
+    user.age = 21
+    user.position = "captain"
+    user.speciality = "research engineer"
+    user.address = "module_1"
+    user.email = "scott_chief@mars.org"
+    user.hashed_password = "cap"
+    session.add(user)
+    session.commit()
+    # app.run()
+
+
+if __name__ == '__main__':
+    main()
