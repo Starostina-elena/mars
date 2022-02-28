9f351e4069b3fdd57493f75e586c4a60b492262b
Лена <starostina.lena05@gmail.com>
2022-02-28 17:01:31 +0400

    sqlalchemy - домашнее задание, последняя задача (регистрация)

--- loginform.py
+++ loginform.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField, EmailField, IntegerField
+from wtforms.validators import DataRequired, EqualTo
 
 from flask import Flask, render_template, redirect
 
@@ -16,12 +16,34 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Доступ')
 
 
+class RegisterForm(FlaskForm):
+    email = EmailField('Login / email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    repeat_password = PasswordField('Repeat password',
+                                    validators=[DataRequired(), EqualTo('password', message='Пароли не совпадают')])
+    surname = StringField('Surname', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    age = IntegerField('Age', validators=[DataRequired()])
+    position = StringField('Position', validators=[DataRequired()])
+    speciality = StringField('Speciality', validators=[DataRequired()])
+    address = StringField('Address', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         return redirect('/success')
     return render_template('login.html', title='Аварийный доступ', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 @app.route('/success')
