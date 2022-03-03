b150c037f36c9e5142811af1535b55865e1a6f0b
Лена <starostina.lena05@gmail.com>
2022-03-03 16:52:25 +0400

    что-то...

--- loginform.py
+++ loginform.py
@@ -1,33 +1,8 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, EmailField, IntegerField
-from wtforms.validators import DataRequired, EqualTo
-
 from flask import Flask, render_template, redirect
+from forms.user import LoginForm, RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-
-
-class LoginForm(FlaskForm):
-    astronaut_name = StringField('ID астронавта', validators=[DataRequired()])
-    astronaut_password = PasswordField('Пароль астронавта', validators=[DataRequired()])
-    captain_name = StringField('ID капитана', validators=[DataRequired()])
-    captain_password = PasswordField('Пароль капитана', validators=[DataRequired()])
-    submit = SubmitField('Доступ')
-
-
-class RegisterForm(FlaskForm):
-    email = EmailField('Login / email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    repeat_password = PasswordField('Repeat password',
-                                    validators=[DataRequired(), EqualTo('password', message='Пароли не совпадают')])
-    surname = StringField('Surname', validators=[DataRequired()])
-    name = StringField('Name', validators=[DataRequired()])
-    age = IntegerField('Age', validators=[DataRequired()])
-    position = StringField('Position', validators=[DataRequired()])
-    speciality = StringField('Speciality', validators=[DataRequired()])
-    address = StringField('Address', validators=[DataRequired()])
-    submit = SubmitField('Submit')
 
 
 @app.route('/login', methods=['GET', 'POST'])
