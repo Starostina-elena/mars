191e632c60a6f66dc9e59606dbbf6d9fc3d91c9e
Лена <starostina.lena05@gmail.com>
2022-03-07 18:03:22 +0400

    rest api - 4 задача из домашней работы

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from requests import put
+from requests import put, get
 
 print(put('http://127.0.0.1:5000/api/jobs/1',
           json={'id': 4,
@@ -10,3 +10,27 @@ print(put('http://127.0.0.1:5000/api/jobs/1',
                 'is_finished': False,
                 'team_leader': 1,
                 'creator': 5}).json())  # корректный запрос
+
+print(put('http://127.0.0.1:5000/api/jobs/10000',
+          json={'id': 4,
+                'job': 'Building main house222',
+                'work_size': 20,
+                'collaborators': '3, 4',
+                'start_date': None,
+                'end_date': None,
+                'is_finished': False,
+                'team_leader': 1,
+                'creator': 5}).json())  # некорректный запрос - нет такого id
+
+print(put('http://127.0.0.1:5000/api/jobs/aaa',
+          json={'id': 4,
+                'job': 'Building main house222',
+                'work_size': 20,
+                'collaborators': '3, 4',
+                'start_date': None,
+                'end_date': None,
+                'is_finished': False,
+                'team_leader': 1,
+                'creator': 5}).json())  # некорректный запрос - id не число
+
+print(get('http://127.0.0.1:5000/api/jobs').json())
