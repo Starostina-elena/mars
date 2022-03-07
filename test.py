4e1154d56a90497c46dd07a53758a89f24367971
Лена <starostina.lena05@gmail.com>
2022-03-07 17:32:04 +0400

    rest api - 2 задача из домашней работы

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-from requests import delete
+from requests import delete, get
 
-print(delete('http://127.0.0.1:5000/api/jobs/100').json())
+print(delete('http://127.0.0.1:5000/api/jobs/1').json())  # корректный запрос
+print(delete('http://127.0.0.1:5000/api/jobs/1000').json())  # некорректный запрос - нет такого id
+print(delete('http://127.0.0.1:5000/api/jobs/aaa').json())  # некорректный запрос - id строка
+
+print(get('http://127.0.0.1:5000/api/jobs').json())
