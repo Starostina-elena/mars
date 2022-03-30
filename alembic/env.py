cf22d7fc7450a71b80aa26eb13b48165a1e0c04c
Лена <starostina.lena05@gmail.com>
2022-03-30 17:44:06 +0400

    правки в env.py (алембик)

--- alembic/env.py
+++ alembic/env.py
@@ -21,7 +21,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 
-sys.path.insert(0, 'C:\Users\Лена\PycharmProjects\mars')
+sys.path.insert(0, 'C://Users/Лена/PycharmProjects/mars')
 from data.db_session import SqlAlchemyBase
 import data.__all_models
 target_metadata = SqlAlchemyBase.metadata
